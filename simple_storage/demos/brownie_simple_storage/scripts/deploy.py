d76281ba54fe75b38aa1806e797ff4bc71ccf982
KevmarPerez <kvnmrbt@gmail.com>
2022-09-17 11:06:23 +0300

    brownie redo

--- simple_storage/demos/brownie_simple_storage/scripts/deploy.py
+++ simple_storage/demos/brownie_simple_storage/scripts/deploy.py
@@ -18,7 +18,7 @@ def deploy_simple_storage():
     print(updated_stored_value)
 
 def get_account():
-    if(network.show_active == "development"):
+    if(network.show_active() == "development"):
         return accounts[0]
     else:
         return accounts.add(config["wallets"]["from_key"])
